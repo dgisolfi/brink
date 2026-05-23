e9a5478b31a3d5ec15e78452bcf5ece828c5d147
dgisolfi3 <gisolfid@outlook.com>
2026-05-23 09:54:47 -0400

    Setup docker development env
    - Created basic dockerfile for dev image to compile / test
    - Added docker-compose to handle volume mount
    - Created initial brink.cpp

---
+++ src/brink.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        return 1;
+    }
+
+    // Check if file exists, open it for editing if so
+
+    // else create a new file
+    
+    return 0;
+}
